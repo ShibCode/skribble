55f5cd959eebd6ee944192b1245808c1a6b41f55
ShibCode <100206484+ShibCode@users.noreply.github.com>
2024-12-17 15:45:25 +0500

    deploy backend

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 
 const URL =
-  import.meta.env.MODE === "production" ? undefined : "http://localhost:8080";
+  import.meta.env.MODE === "production"
+    ? "https://skribble-0hzb.onrender.com"
+    : "http://localhost:8080";
 
 export const socket = io(URL);
